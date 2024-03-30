d8213fd92208058ee3d1ad5db141146c30cfd37e
Yzak Carmo <yzak.carmo@gmail.com>
2024-03-30 13:25:18 -0300

    Adicionado o SellerDaoJDBC e o DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,9 +10,9 @@ import java.time.LocalDate;
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(5,"Toys");
-        System.out.println(obj);
-
         Seller seller = new Seller(1,"Joao", "joao@joao", LocalDate.now(),5000.0,obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
     }
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,33 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+    @Override
+    public void insert(Seller dep) {
+
+    }
+
+    @Override
+    public void update(Seller dep) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public Seller findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
